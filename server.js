1a2f892db39d5e96304057b9f3488e3a2d8c3975
Rahul Panchal <rm5901960@gmail.com>
2024-03-17 17:15:48 +0530

    Adding array

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const https = require('https');
 
 const app = express();
 
-// Define the URL of your website to monitor
-const websiteUrl = 'https://chatbison.onrender.com/';
+// Define the URLs of your websites to monitor
+const websiteUrls = [
+    'https://chatbison.onrender.com/',
+    'https://pingit.onrender.com'
+    // Add more website URLs as needed
+];
 
-// Function to ping the website with a random interval
-const pingWebsite = () => {
+// Function to ping a website with a random interval
+const pingWebsite = (websiteUrl) => {
     const protocol = websiteUrl.startsWith('https') ? https : http;
     const options = {
         method: 'GET',
@@ -19,28 +23,39 @@ const pingWebsite = () => {
 
     const req = protocol.request(websiteUrl, options, (res) => {
         var randomInterval;
-        
 
         // Generate a random time interval between 1 to 5 minutes (60000 milliseconds = 1 minute)
-        randomInterval = Math.floor(Math.random() * (2 * 60000 - 1 * 60000 + 1)) + 1 * 60000;
-        setTimeout(pingWebsite, randomInterval);
+        randomInterval = Math.floor(Math.random() * (5 * 60000 - 1 * 60000 + 1)) + 1 * 60000;
+        setTimeout(() => pingWebsite(websiteUrl), randomInterval);
 
         if (res.statusCode === 200) {
-            const time=new Date();
+            const time = new Date();
             console.log(`${time.toLocaleString()}: Website ${websiteUrl} is UP, next check after ${randomInterval/60000}m.`);
         } else {
-            console.log(`Website ${websiteUrl} is DOWN. Status code: ${res.statusCode}`);
+            console.log(`[${getCurrentTime()}] Website ${websiteUrl} is DOWN. Status code: ${res.statusCode}, next check after ${randomInterval/60000}m.`);
         }
+        console.log("\n--------------------------------\n")
     });
 
     req.on('error', (error) => {
         console.error(`Error pinging website ${websiteUrl}: ${error.message}`);
-
-        // If an error occurs, retry after a fixed interval (e.g., 1 minute)
-        setTimeout(pingWebsite, 1 * 60000);
+        setTimeout(() => pingWebsite(websiteUrl), 1 * 60000);
     });
 
     req.end();
+};
+
+// Function to ping all websites
+const pingAllWebsites = () => {
+    websiteUrls.forEach((url) => {
+        pingWebsite(url);
+    });
+};
+
+// Function to get current time in readable format
+const getCurrentTime = () => {
+    const currentTime = new Date();
+    return currentTime.toLocaleString();
 };
 
 // Endpoint to check the status of the monitor
@@ -53,5 +68,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-// Initial ping on startup
-pingWebsite();
+// Initial ping of all websites on startup
+pingAllWebsites();
