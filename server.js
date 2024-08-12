eb413b17f009b18840716e0817b4321133af3722
Rahul Panchal <95669983+rahulpanchal0106@users.noreply.github.com>
2024-08-12 21:44:18 +0530

    Update foxus-ai

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Define the URLs of your websites to monitor
 const websiteUrls = [
     process.env.PROD,
-    'https://chatbison.onrender.com/',
+    'https://foxus-ai-backend.onrender.com',
     'https://socio-server-01re.onrender.com'
     
 ];
